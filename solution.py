a79caf48b6d62e1e643410afb7e51cf08c908fc3
Niharika Veggalam <veggalamniharika94@gmail.com>
2026-08-02 22:25:01 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,11 @@
-
+# Algorithm:
+# 1. Read N.
+# 2. Store passengers.
+# 3. Check senior citizens:
+#       If age > 60 and current != destination:
+#             print -1
+#             stop
+# 4. Extract movable passengers.
+# 5. Convert current arrangement into destination positions.
+# 6. Count inversions using merge sort.
+# 7. Print answer.

ac57f4ecbe102c733983f2638d1436a08b9c9ff4
Niharika Veggalam <veggalamniharika94@gmail.com>
2026-08-02 22:26:33 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,3 +9,17 @@
 # 5. Convert current arrangement into destination positions.
 # 6. Count inversions using merge sort.
 # 7. Print answer.
+
+
+
+# Step 1: Read N
+N = int(input())
+
+# Step 2: Store passengers
+passengers = []
+
+for _ in range(N):
+    current, destination, age = map(int, input().split())
+    passengers.append((current, destination, age))
+
+print(passengers)
